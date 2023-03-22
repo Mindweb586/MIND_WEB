672e208c6eb8d3624d295ac2323b8e167f439e38
ANKIT1610 <aab2@somaiya.edu>
2023-03-22 14:21:26 +0530

    Alpha song me pause button lagaya h

--- js_files/alpha-song.js
+++ js_files/alpha-song.js
@@ -2,33 +2,44 @@ let left = document.getElementById("left")
 let centre = document.getElementById("centre")
 let right = document.getElementById("right")
 let song = document.getElementById("audio")
-let btn = document.getElementById("play_btn")
+// let btn = document.getElementById("play_btn")
 let playbtn = document.getElementById("playbutton")
-let resetbtn = document.getElementById("resetbutton")
+let pause = document.getElementById("pausebutton")
 // const imgobj =list[0]
+let interval
 
-
+let i=0
 
 playbtn.addEventListener('click',()=>{
     song.play()
-    let i=0
-    setInterval(() => {
-        imgobj = list[i]
-        left.innerHTML = `<img src=${imgobj[0]} />`
-        right.innerHTML=    `<img src=${imgobj[0]} />`
-        centre.innerHTML=   `<img src=${imgobj[1]} />`
-        i++
-    }, 2350);
-    
-})
-
-resetbtn.addEventListener('click',()=>{
-    song.pause()
-    let i=0
+    interval = setInterval(() => {
     imgobj = list[i]
     left.innerHTML = `<img src=${imgobj[0]} />`
     right.innerHTML=    `<img src=${imgobj[0]} />`
     centre.innerHTML=   `<img src=${imgobj[1]} />`
+    i++
+}, 2350);
+    return interval
+})
+
+pause.addEventListener('click',()=>{
+    song.pause()
+    clearInterval(interval);
 })
 
 
+// song.pause()
+// imgobj = list[a]
+// left.innerHTML = `<img src=${imgobj[0]} />`
+// right.innerHTML=    `<img src=${imgobj[0]} />`
+// centre.innerHTML=   `<img src=${imgobj[1]} />`
+
+// song.play()
+// let i=0
+// setInterval(() => {
+//     imgobj = list[i]
+//     left.innerHTML = `<img src=${imgobj[0]} />`
+//     right.innerHTML=    `<img src=${imgobj[0]} />`
+//     centre.innerHTML=   `<img src=${imgobj[1]} />`
+//     i++
+// }, 2350);
